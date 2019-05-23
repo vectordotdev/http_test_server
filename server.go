ed39ce86b13cf41feca8695f31ed5cf18dd23fe0
Ben Johnson <bjohnson@binarylogic.com>
2019-05-23 17:22:00 -0400

    Update server to handle logging

--- server.go
+++ server.go
@@ -1,28 +1,138 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"html"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync/atomic"
+	"time"
+)
+
+type key int
+
+const (
+	requestIDKey key = 0
+)
+
+var (
+	healthy int32
 )
 
 type Server struct {
-	RequestCount int64
+	address      string
 	File         *os.File
-	Port         string
+	logger       *log.Logger
+	RequestCount int64
+	server       *http.Server
 }
 
 func (s *Server) Listen() {
-	http.ListenAndServe(":"+s.Port, nil)
+	done := make(chan bool)
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		s.logger.Println("Server is shutting down...")
+		atomic.StoreInt32(&healthy, 0)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		s.server.SetKeepAlivesEnabled(false)
+		if err := s.server.Shutdown(ctx); err != nil {
+			s.logger.Fatalf("Could not gracefully shutdown the server: %v\n", err)
+		}
+		close(done)
+	}()
+
+	s.logger.Println("Server is ready to handle requests at", s.address)
+	atomic.StoreInt32(&healthy, 1)
+	if err := s.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		s.logger.Fatalf("Could not listen on %s: %v\n", s.address, err)
+	}
+
+	<-done
+	s.logger.Println("Server stopped")
 }
 
 func NewServer(port string, filePath string) *Server {
-	server := &Server{RequestCount: 0}
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	logger.Println("Server is starting...")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	router := http.NewServeMux()
+	router.Handle("/", index())
+	router.Handle("/_health", health())
+
+	nextRequestID := func() string {
+		return fmt.Sprintf("%d", time.Now().UnixNano())
+	}
+
+	httpServer := &http.Server{
+		Addr:         ":" + port,
+		Handler:      tracing(nextRequestID)(logging(logger)(router)),
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	server := &Server{address: ":" + port, logger: logger, server: httpServer, RequestCount: 0}
 
 	return server
+}
+
+func index() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "Hello, World!")
+	})
+}
+
+func health() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if atomic.LoadInt32(&healthy) == 1 {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		w.WriteHeader(http.StatusServiceUnavailable)
+	})
+}
+
+func logging(logger *log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				requestID, ok := r.Context().Value(requestIDKey).(string)
+				if !ok {
+					requestID = "unknown"
+				}
+				logger.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
+			}()
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func tracing(nextRequestID func() string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			requestID := r.Header.Get("X-Request-Id")
+			if requestID == "" {
+				requestID = nextRequestID()
+			}
+			ctx := context.WithValue(r.Context(), requestIDKey, requestID)
+			w.Header().Set("X-Request-Id", requestID)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
 }
