4785c39623b505d4d97f147cc0abece81567f62a
Bruce Guenter <bruce@timber.io>
2020-09-22 11:17:39 -0600

    Break up concurrency suite and allow running single tests
    Signed-off-by: Bruce Guenter <bruce@timber.io>

---
+++ bin/concurrency/suite/natural_added_load.sh
@@ -0,0 +1,3 @@
+HTTP_TEST_DESCRIPTION="Natural rate limit, additional load added mid-test"
+HTTP_TEST_LATENCY_DISTRIBUTION="EXPRESSION"
+HTTP_TEST_LATENCY_EXPRESSION_MEAN_MS="200 + 10 * (sin((1/60) * 2 * pi * t) > 0 ? (active_requests<5 ? active_requests : (5-1) + 2 ** (active_requests - 5)) : 2 ** (active_requests - 1))"
---
+++ bin/concurrency/suite/natural_low_slope.sh
@@ -0,0 +1,3 @@
+HTTP_TEST_DESCRIPTION="Natural rate limit, low load slope"
+HTTP_TEST_LATENCY_DISTRIBUTION="EXPRESSION"
+HTTP_TEST_LATENCY_EXPRESSION_MEAN_MS="200 + active_requests * 5 + (0<=active_requests && active_requests<10 ? 0 : 2 ** (active_requests - 10))"
