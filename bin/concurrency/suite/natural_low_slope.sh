91858306f7d99f19e492ad95fc66e61c697da135
Bruce Guenter <bruce.guenter@datadoghq.com>
2021-08-06 16:47:29 -0600

    Scale the concurrency request rate graph to an "expected" level

--- bin/concurrency/suite/natural_low_slope.sh
+++ bin/concurrency/suite/natural_low_slope.sh
@@ -1,3 +1,4 @@
 HTTP_TEST_DESCRIPTION="Natural rate limit, low load slope"
 HTTP_TEST_LATENCY_DISTRIBUTION="EXPRESSION"
 HTTP_TEST_LATENCY_EXPRESSION_MEAN_MS="200 + active_requests * 5 + (0<=active_requests && active_requests<10 ? 0 : 2 ** (active_requests - 10))"
+HTTP_TEST_EXPECTED_RATE=30
